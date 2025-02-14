15d19602d225aaf1e8368f00d8252eb9e07059c4
younes <benjdidawiyad@gmail.com>
2025-02-14 05:11:44 +0100

    addin delete and create for categories

--- public/index.php
+++ public/index.php
@@ -5,29 +5,29 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use app\Controller\Aadoui;
 use app\Controller\AuthController;
-<<<<<<< HEAD
+use app\Controller\CategorieController;
 use app\Controller\ClientController;
-=======
->>>>>>> ff45fba1327a113e160bb708362fdadc15780a3a
 use app\Controller\OffreController;
 use app\Controller\SiteController;
+use app\Controller\TagController;
 use app\Core\application;   
 session_start();
 $app = new Application(dirname(__DIR__));
-$app->router->get('/',[SiteController::class,"AdminDashboard"]);
+// $app->router->get('',[SiteController::class,"AdminDashboard"]);
 $app->router->get('/contact',[SiteController::class,"ClientDashboard"]);
 $app->router->post('/contact',[OffreController::class,"create"]);
 $app->router->get('/contact/delete',[OffreController::class,"DeleteOffre"]);
 $app->router->get('/register',[AuthController::class,"register"]);
 $app->router->post('/register',[AuthController::class,"register"]);
 $app->router->get('/test',[SiteController::class,"test"]);
-$app->router->get('/Login',[AuthController::class,"login"]);
-$app->router->post('/Login',[AuthController::class,"login"]);
-<<<<<<< HEAD
-
-=======
+$app->router->get('/',[AuthController::class,"login"]);
+$app->router->post('/',[AuthController::class,"login"]);
 $app->router->get('/offres',[OffreController::class,"viewsOffres"]);
->>>>>>> ff45fba1327a113e160bb708362fdadc15780a3a
+$app->router->get('/categories',[CategorieController::class,"renderCategories"]);
+$app->router->post('/categories',[CategorieController::class,"create"]);
 
+$app->router->get('/Tags',[TagController::class,"showTags"]);
+// $app->router->get('/Tags',[TagController::class,"create"]);
+$app->router->post('/Tags',[TagController::class,"create"]);
 $app->run();
 ?>
